632e99e38d6d1fd1f2a5273789b36854474f733d
Robert <bob@rwscott.co.uk>
2023-05-30 12:29:44 +0100

    Add Logo component

---
+++ src/components/logo.rs
@@ -0,0 +1,13 @@
+use yew::prelude::*;
+
+#[derive(Properties, PartialEq)]
+pub struct Props {
+    pub src: AttrValue,
+    pub alt: AttrValue,
+}
+#[function_component(Logo)]
+pub fn logo(props: &Props) -> Html {
+    html! {
+          <img class="mx-auto h-10 w-auto" src={props.src} alt={props.alt}/>
+    }
+}
